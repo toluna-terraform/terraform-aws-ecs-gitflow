4264b0809cd83f33ba4a8daa7a0fe3fab02cbfe7
EliMeitner <eli.meitner@toluna.com>
2022-01-31 13:53:53 +0200

    Update data.tf

--- modules/build/data.tf
+++ modules/build/data.tf
@@ -35,6 +35,7 @@ data "aws_iam_policy_document" "codebuild_role_policy" {
             "ssm:*",
             "ecs:DescribeTaskDefinition",
             "cloudformation:*",
+            "codebuild:*",
             "s3:*",
             "apigateway:*",
             "lambda:*"
