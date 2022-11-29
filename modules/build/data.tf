645ff2d4c98c1743cbc8c1d189491166f28fabf3
EliMeitner <eli.meitner@toluna.com>
2022-11-29 06:48:22 +0200

    add vpc for sonar support

--- modules/build/data.tf
+++ modules/build/data.tf
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "codebuild_role_policy" {
   statement {
     actions   = [
             "iam:*",
-             "ec2:*",
+            "ec2:*",
             "logs:CreateLogGroup",
             "logs:CreateLogStream",
             "logs:PutLogEvents",
