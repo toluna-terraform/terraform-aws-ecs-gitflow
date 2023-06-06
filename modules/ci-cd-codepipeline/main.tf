7e247b993d5eca5d3db4d363c1dd82fdebf2bb5a
EliMeitner <eli.meitner@toluna.com>
2023-06-06 13:16:36 +0300

    add cloudTrail permissions

--- modules/ci-cd-codepipeline/main.tf
+++ modules/ci-cd-codepipeline/main.tf
@@ -157,3 +157,23 @@ resource "aws_cloudwatch_event_target" "trigger_pipeline" {
   arn       = aws_codepipeline.codepipeline.arn
   role_arn = aws_iam_role.codepipeline_role.arn
 }
+
+resource "aws_cloudwatch_log_group" "trigger_pipeline" {
+  name = aws_cloudwatch_event_rule.trigger_pipeline.name
+  retention_in_days = 3
+}
+
+resource "aws_cloudtrail" "trigger_pipeline" {
+  name = "${local.codepipeline_name}-cloud-trail"
+  s3_bucket_name = "${var.s3_bucket}"
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.trigger_pipeline.arn}:*"
+  event_selector {
+    read_write_type           = "WriteOnly"
+    include_management_events = true
+
+    data_resource {
+      type   = "AWS::S3::Object"
+      values = ["arn:aws:s3:::${var.s3_bucket}/${var.env_name}/source_artifacts.zip"]
+    }
+  }
+}
