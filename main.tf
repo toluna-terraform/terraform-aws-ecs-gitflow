ff6bc4afa3aa4003680f80525e93ef41134c5490
EliMeitner <eli.meitner@toluna.com>
2024-02-07 12:56:39 +0200

    remove default nullalbe

--- main.tf
+++ main.tf
@@ -1,20 +1,37 @@
 locals {
-  image_uri = "${var.ecr_repo_url}:${var.from_env}"
-  artifacts_bucket_name = "s3-codepipeline-${var.app_name}-${var.env_type}"
-  run_tests = var.run_integration_tests || var.run_stress_tests ? true : false
+  app_name               = var.app_name == null ? var.pipeline_config.app_name : var.app_name
+  ecr_repo_url           = var.ecr_repo_url == null ? var.pipeline_config.ecr_repo_url : var.ecr_repo_url
+  ecr_repo_name          = var.ecr_repo_name == null ? var.pipeline_config.ecr_repo_name : var.ecr_repo_name
+  from_env               = var.from_env == null ? var.pipeline_config.from_env : var.from_env
+  env_type               = var.env_type == null ? var.pipeline_config.env_type : var.env_type
+  env_name               = var.env_name == null ? var.pipeline_config.env_name : var.env_name
+  pipeline_type          = var.pipeline_type == null ? var.pipeline_config.pipeline_type : var.pipeline_type
+  ecs_service_name       = var.ecs_service_name == null ? var.pipeline_config.ecs_service_name : var.ecs_service_name
+  ecs_cluster_name       = var.ecs_cluster_name == null ? var.pipeline_config.ecs_cluster_name : var.ecs_cluster_name
+  task_def_name          = var.task_def_name == null ? var.pipeline_config.task_def_name : var.task_def_name
+  run_integration_tests  = var.run_integration_tests == null ? var.pipeline_config.run_integration_tests : var.run_integration_tests
+  test_report_group      = var.test_report_group == null ? var.pipeline_config.test_report_group : var.test_report_group
+  coverage_report_group  = var.coverage_report_group == null ? var.pipeline_config.coverage_report_group : var.coverage_report_group
+  enable_jira_automation = var.enable_jira_automation == null ? var.pipeline_config.enable_jira_automation : var.enable_jira_automation
+  dockerfile_path        = var.dockerfile_path == null ? "service/${local.app_name}" : var.dockerfile_path
+  source_repository      = var.source_repository == null ? "tolunaengineering/${local.app_name}" : var.source_repository
+  artifacts_bucket_name  = "s3-codepipeline-${local.app_name}-${local.env_type}"
+  run_tests              = local.run_integration_tests || var.run_stress_tests ? true : false
+  image_uri              = "${local.ecr_repo_url}:${local.from_env}"
+  vpc_config             = var.vpc_config.vpc_id == "NULL" ? merge(var.pipeline_config.vpc_config, { security_group_ids = var.security_group_ids }) : var.vpc_config
 }
 
 module "ci-cd-code-pipeline" {
-  source                       = "./modules/ci-cd-codepipeline"
-  env_name                     = var.env_name
-  app_name                     = var.app_name
-  pipeline_type                = var.pipeline_type
-  source_repository            = var.source_repository
-  s3_bucket                    = local.artifacts_bucket_name
-  build_codebuild_projects     = [module.build.attributes.name]
-  post_codebuild_projects      = [module.post.attributes.name]
-  pre_codebuild_projects       = [module.pre.attributes.name]
-  code_deploy_applications     = [module.code-deploy.attributes.name]
+  source                   = "./modules/ci-cd-codepipeline"
+  env_name                 = local.env_name
+  app_name                 = local.app_name
+  pipeline_type            = local.pipeline_type
+  source_repository        = local.source_repository
+  s3_bucket                = local.artifacts_bucket_name
+  build_codebuild_projects = [module.build.attributes.name]
+  post_codebuild_projects  = [module.post.attributes.name]
+  pre_codebuild_projects   = [module.pre.attributes.name]
+  code_deploy_applications = [module.code-deploy.attributes.name]
 
   depends_on = [
     module.build,
@@ -27,92 +44,92 @@ module "ci-cd-code-pipeline" {
 
 module "build" {
   source                                = "./modules/build"
-  env_name                              = var.env_name
-  env_type                              = var.env_type
-  codebuild_name                        = "build-${var.app_name}"
-  source_repository                     = var.source_repository
+  env_name                              = local.env_name
+  env_type                              = local.env_type
+  codebuild_name                        = "build-${local.app_name}"
+  source_repository                     = local.source_repository
   codebuild_env_instance_type           = var.codebuild_env_instance_type
   s3_bucket                             = local.artifacts_bucket_name
   privileged_mode                       = true
   environment_variables_parameter_store = var.environment_variables_parameter_store
-  vpc_config                            = var.vpc_config
-  environment_variables                 = merge(var.environment_variables, { APPSPEC = templatefile("${path.module}/templates/appspec.json.tpl", { APP_NAME = "${var.app_name}", ENV_TYPE = "${var.env_type}", HOOKS = local.run_tests, PIPELINE_TYPE = var.pipeline_type})}) //TODO: try to replace with file
-  buildspec_file                        = templatefile("buildspec.yml.tpl", 
-  { APP_NAME = var.app_name,
-    ENV_TYPE = var.env_type,
-    ENV_NAME = var.env_name,
-    PIPELINE_TYPE = var.pipeline_type,
-    IMAGE_URI = var.pipeline_type == "dev" ? "${var.ecr_repo_url}:${var.env_name}" : local.image_uri, 
-    DOCKERFILE_PATH = var.dockerfile_path, 
-    ECR_REPO_URL = var.ecr_repo_url, 
-    ECR_REPO_NAME = var.ecr_repo_name,
-    TASK_DEF_NAME = var.task_def_name, 
-    ADO_USER = data.aws_ssm_parameter.ado_user.value, 
-    ADO_PASSWORD = data.aws_ssm_parameter.ado_password.value,
-    TEST_REPORT = var.test_report_group,
-    CODE_COVERAGE_REPORT = var.coverage_report_group
+  vpc_config                            = local.vpc_config
+  environment_variables                 = merge(var.environment_variables, { APPSPEC = templatefile("${path.module}/templates/appspec.json.tpl", { APP_NAME = "${local.app_name}", ENV_TYPE = "${local.env_type}", HOOKS = local.run_tests, PIPELINE_TYPE = local.pipeline_type }) }) //TODO: try to replace with file
+  buildspec_file = templatefile("buildspec.yml.tpl",
+    { APP_NAME             = local.app_name,
+      ENV_TYPE             = local.env_type,
+      ENV_NAME             = local.env_name,
+      PIPELINE_TYPE        = local.pipeline_type,
+      IMAGE_URI            = local.pipeline_type == "dev" ? "${local.ecr_repo_url}:${local.env_name}" : local.image_uri,
+      DOCKERFILE_PATH      = local.dockerfile_path,
+      ECR_REPO_URL         = local.ecr_repo_url,
+      ECR_REPO_NAME        = local.ecr_repo_name,
+      TASK_DEF_NAME        = local.task_def_name,
+      ADO_USER             = data.aws_ssm_parameter.ado_user.value,
+      ADO_PASSWORD         = data.aws_ssm_parameter.ado_password.value,
+      TEST_REPORT          = local.test_report_group,
+      CODE_COVERAGE_REPORT = local.coverage_report_group
   })
 }
 
 
 module "code-deploy" {
-  source             = "./modules/codedeploy"
-  env_name           = var.env_name
-  env_type           = var.env_type
-  app_name           = var.app_name
-  s3_bucket          = "s3-codepipeline-${var.app_name}-${var.env_type}"
-  ecs_service_name   = var.ecs_service_name
-  ecs_cluster_name   = var.ecs_cluster_name
-  alb_listener_arn   = var.alb_listener_arn
-  alb_test_listener_arn   = var.alb_test_listener_arn
-  alb_tg_blue_name   = var.alb_tg_blue_name
-  alb_tg_green_name  = var.alb_tg_green_name
-  ecs_iam_roles_arns = var.ecs_iam_roles_arns
+  source                           = "./modules/codedeploy"
+  env_name                         = local.env_name
+  env_type                         = local.env_type
+  app_name                         = local.app_name
+  s3_bucket                        = "s3-codepipeline-${local.app_name}-${local.env_type}"
+  ecs_service_name                 = local.ecs_service_name
+  ecs_cluster_name                 = local.ecs_cluster_name
+  alb_listener_arn                 = var.alb_listener_arn
+  alb_test_listener_arn            = var.alb_test_listener_arn
+  alb_tg_blue_name                 = var.alb_tg_blue_name
+  alb_tg_green_name                = var.alb_tg_green_name
+  ecs_iam_roles_arns               = var.ecs_iam_roles_arns
   termination_wait_time_in_minutes = var.termination_wait_time_in_minutes
 }
 
 
 module "pre" {
   source                                = "./modules/pre"
-  env_name                              = var.env_name
-  env_type                              = var.env_type
-  codebuild_name                        = "pre-${var.app_name}"
+  env_name                              = local.env_name
+  env_type                              = local.env_type
+  codebuild_name                        = "pre-${local.app_name}"
   source_repository                     = var.source_repository
-  s3_bucket                             = "s3-codepipeline-${var.app_name}-${var.env_type}"
+  s3_bucket                             = "s3-codepipeline-${local.app_name}-${local.env_type}"
   privileged_mode                       = true
   environment_variables_parameter_store = var.environment_variables_parameter_store
-  environment_variables                 = merge(var.environment_variables, { APPSPEC = templatefile("${path.module}/templates/appspec.json.tpl", { APP_NAME = "${var.app_name}", ENV_TYPE = "${var.env_type}", HOOKS = var.run_integration_tests, PIPELINE_TYPE = var.pipeline_type})})
-  buildspec_file                        = templatefile("${path.module}/templates/pre_buildspec.yml.tpl", 
-  { ENV_NAME = var.env_name,
-    APP_NAME = var.app_name,
-    ENV_TYPE = var.env_type,
-    PIPELINE_TYPE = var.pipeline_type,
-    FROM_ENV = var.from_env,
-    ECR_REPO_URL = var.ecr_repo_url, 
-    ECR_REPO_NAME = var.ecr_repo_name,
-    TASK_DEF_NAME = var.task_def_name 
-    })
+  environment_variables                 = merge(var.environment_variables, { APPSPEC = templatefile("${path.module}/templates/appspec.json.tpl", { APP_NAME = "${local.app_name}", ENV_TYPE = "${local.env_type}", HOOKS = local.run_integration_tests, PIPELINE_TYPE = local.pipeline_type }) })
+  buildspec_file = templatefile("${path.module}/templates/pre_buildspec.yml.tpl",
+    { ENV_NAME      = local.env_name,
+      APP_NAME      = local.app_name,
+      ENV_TYPE      = local.env_type,
+      PIPELINE_TYPE = local.pipeline_type,
+      FROM_ENV      = local.from_env,
+      ECR_REPO_URL  = local.ecr_repo_url,
+      ECR_REPO_NAME = local.ecr_repo_name,
+      TASK_DEF_NAME = local.task_def_name
+  })
 }
 
 
 module "post" {
   source                                = "./modules/post"
-  env_name                              = var.env_name
-  env_type                              = var.env_type
-  codebuild_name                        = "post-${var.app_name}"
+  env_name                              = local.env_name
+  env_type                              = local.env_type
+  codebuild_name                        = "post-${local.app_name}"
   source_repository                     = var.source_repository
-  s3_bucket                             = "s3-codepipeline-${var.app_name}-${var.env_type}"
+  s3_bucket                             = "s3-codepipeline-${local.app_name}-${local.env_type}"
   privileged_mode                       = true
   environment_variables_parameter_store = var.environment_variables_parameter_store
-  enable_jira_automation                = var.enable_jira_automation
+  enable_jira_automation                = local.enable_jira_automation
 
-  buildspec_file                        = templatefile("${path.module}/templates/post_buildspec.yml.tpl", 
-  { ECR_REPO_URL = var.ecr_repo_url, 
-    ECR_REPO_NAME = var.ecr_repo_name,
-    ENV_NAME = split("-",var.env_name)[0],
-    FROM_ENV = var.from_env,
-    APP_NAME = var.app_name,
-    ENV_TYPE = var.env_type,
-    ENABLE_JIRA_AUTOMATION = var.enable_jira_automation
-    })
+  buildspec_file = templatefile("${path.module}/templates/post_buildspec.yml.tpl",
+    { ECR_REPO_URL           = local.ecr_repo_url,
+      ECR_REPO_NAME          = local.ecr_repo_name,
+      ENV_NAME               = local.env_name,
+      FROM_ENV               = local.from_env,
+      APP_NAME               = local.app_name,
+      ENV_TYPE               = local.env_type,
+      ENABLE_JIRA_AUTOMATION = local.enable_jira_automation
+  })
 }
--- variables.tf
+++ variables.tf
@@ -1,107 +1,124 @@
+variable "pipeline_config" {
+}
+
 variable "env_name" {
-    type = string
+  type     = string
+  default  = null
 }
 
 variable "from_env" {
-    type = string
+  type     = string
+  default  = null
 }
 
 variable "app_name" {
-    type = string
+  type     = string
+  default  = null
 }
 
 variable "env_type" {
-    type = string
+  type     = string
+  default  = null
 }
 
 variable "run_integration_tests" {
-    type = bool
-    default = false
+  type     = bool
+  default  = null
 }
 
 variable "run_stress_tests" {
-    type = bool
-    default = false
+  type    = bool
+  default = false
 }
 
 variable "ecr_repo_url" {
-    type = string 
+  type     = string
+  default  = null
 }
 
 variable "ecr_registry_id" {
-    type = string
+  type     = string
+  default  = null
 }
 
 variable "task_def_name" {
-    type = string
+  type     = string
+  default  = null
 }
 
 variable "source_repository" {
-    type = string
+  type     = string
+  default  = null
 }
 
 variable "trigger_branch" {
-    type     = string
- }
+  type     = string
+  default  = null
+}
 
 variable "dockerfile_path" {
-    type = string
-} 
+  type     = string
+  default  = null
+}
 
- variable "ecs_cluster_name" {
-     type = string
- }
+variable "ecs_cluster_name" {
+  type     = string
+  default  = null
+}
 
 variable "ecs_service_name" {
-     type = string
- }
+  type     = string
+  default  = null
+}
 
 variable "alb_listener_arn" {
-     type = string
- }
-
-variable "alb_test_listener_arn" {
-     type = string
- }
- 
-variable "alb_tg_blue_name" {
-     type = string
- }
-
-variable "alb_tg_green_name" {
-     type = string
- }
-
-variable "ecs_iam_roles_arns" {
-     type = list(string)
-     default = ["arn:aws:iam::047763475875:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"]
- }
-
- variable "ecr_repo_name" {
-     type = string
- }
-
-variable "environment_variables_parameter_store" {
- type = map(string)
- default = {
-    "ADO_USER" = "/app/ado_user",
-    "ADO_PASSWORD" = "/app/ado_password"
- }
-}
-
-variable "environment_variables" {
- type = map(string)
- default = {
- }
-}
-
-variable "pipeline_type" {
   type = string
 }
 
+variable "alb_test_listener_arn" {
+  type = string
+}
+
+variable "alb_tg_blue_name" {
+  type = string
+}
+
+variable "alb_tg_green_name" {
+  type = string
+}
+
+variable "ecs_iam_roles_arns" {
+  type    = list(string)
+  default = ["arn:aws:iam::047763475875:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"]
+}
+
+variable "ecr_repo_name" {
+  type     = string
+  default  = null
+}
+
+variable "environment_variables_parameter_store" {
+  type = map(string)
+  default = {
+    "ADO_USER"     = "/app/ado_user",
+    "ADO_PASSWORD" = "/app/ado_password"
+  }
+}
+
+variable "environment_variables" {
+  type = map(string)
+  default = {
+  }
+}
+
+variable "pipeline_type" {
+  type     = string
+  default  = null
+}
+
 variable "codebuild_env_instance_type" {
-    type = string
-    default = "BUILD_GENERAL1_SMALL"
+  type    = string
+  default = "BUILD_GENERAL1_SMALL"
 }
 
 variable "termination_wait_time_in_minutes" {
@@ -109,19 +126,30 @@ variable "termination_wait_time_in_minutes" {
 }
 
 variable "test_report_group" {
-  type = string
+  type     = string
+  default  = null
 }
 
 variable "coverage_report_group" {
-  type = string
+  type     = string
+  default  = null
 }
 
 variable "enable_jira_automation" {
-  type = bool
+  type        = bool
   description = "flag to indicate if Jira automation is enabled"
-  default = false
+  default     = null
 }
 
 variable "vpc_config" {
-  default = {}
+  default = {
+     vpc_id             = "NULL",
+      subnets            = [],
+      security_group_ids = []
+  }
+}
+
+variable "security_group_ids" {
+  type = list(string)
+  default = []
 }
